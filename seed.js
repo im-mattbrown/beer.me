226544124e75f9e2877f24eb040363a2a32ffaa7
mattericbrown <matt.eric.brown@gmail.com>
2016-07-20 23:25:14 -0700

    hard coded beerPlaces API in seed.js

---
+++ seed.js
@@ -0,0 +1,390 @@
+
+
+
+var beerPlaces= [
+  {
+    name: '21st Amendment Brewery Cafe',
+    location: '563 Second St. San Francisco, CA 94107',
+    lat: 37.782257,
+    long: -122.392395,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Ales Unlimited',
+    location: '2398 Webster St. San Francisco, CA 94115',
+    lat: 37.792579,
+    long: -122.432745,
+    rating: 0,
+    type: ['bottleshop']
+  },
+  {
+    name: 'Almanac Beer Co',
+    location: '2325 3rd St. San Francisco, CA 94107',
+    lat: 37.760059,
+    long: -122.388303,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Anchor Brewing Co.',
+    location: '1705 Mariposa St. San Francisco, CA 94107',
+    lat: 37.763718,
+    long: -122.400528,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Armstrong Brewing Company',
+    location: '415 Grand Ave, San Francisco, CA 94080',
+    lat: 37.6556785,
+    long: -122.4831321,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Barebottle Brewing Company',
+    location: '1525 Cortland Ave, San Francisco, CA 94110',
+    lat: 37.7400643,
+    long: -122.479043,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Barrel Head Brewhouse',
+    location: '1785 Fulton St. San Francisco, CA 94117',
+    lat: 37.7757363,
+    long: -122.516138,
+    rating: 0,
+    type: ['brewpub']
+  },
+  {
+    name: 'Beach Chalet and Park Chalet',
+    location: '1000 Great Highway San Francisco, CA 94121',
+    lat: 37.769400,
+    long: -122.510550,
+    rating: 0,
+    type: ['brewpub']
+  },
+  {
+    name: 'The Beer Hall',
+    location: '1 Polk St. San Francisco, CA 94102',
+    lat: 37.7767873,
+    long: -122.4881005,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: 'Black Hammer Brewing',
+    location: '544 Bryant St. San Francisco, CA 94107',
+    lat: 37.7806838,
+    long: -122.4671149,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Black Sands Brewery',
+    location: '701 Haight St. San Francisco, CA 94117',
+    lat: 37.7714343,
+    long: -122.503895,
+    rating: 0,
+    type: ['brewpub']
+  },
+  {
+    name: 'Blueprint Tap Room',
+    location: '680 8th St. San Francisco, CA 94103',
+    lat: 37.770366,
+    long: -122.404629,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Brewcade',
+    location: 'B, 2200 Market St. San Francisco, CA 94114',
+    lat: 37.7657703,
+    long: -122.501557,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'City Beer Store and Tasting Room',
+    location: '1168 Folsom St. San Francisco, CA 94103',
+    lat: 37.775768,
+    long: -122.409409,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: 'Cellarmaker Brewing Co',
+    location: '1150 Howard St. San Francisco, CA 94103',
+    lat: 37.777218,
+    long: -122.410768,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Elizabeth Street Brewery ESB',
+    location: '798 Elizabeth St. San Francisco, CA 94114',
+    lat: 37.752047,
+    long: -122.438425,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Fat Angel',
+    location: "1740 O'Farrell St. San Francisco, CA 94115",
+    lat: 37.783342,
+    long: -122.433182,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: 'Fort Point Beer Company',
+    location: '644 Old Mason St. San Francisco, CA 94129',
+    lat: 37.8030566,
+    long: -122.5348943,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Gestalt Haus',
+    location: '3159 16th St. San Francisco, CA 94110',
+    lat: 37.764851,
+    long: -122.423476,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Golden Gate Tap Room',
+    location: '525 Sutter St. #2 San Francisco, CA 94108',
+    lat: 37.788933,
+    long: -122.408957,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Harmonic Brewing',
+    location: '1050 26th St. San Francisco, CA 94107',
+    lat: 37.7516029,
+    long: -122.4600715,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Hogwash',
+    location: '582 Sutter St. San Francisco, CA 94108',
+    lat: 37.7891647,
+    long: -122.4799598,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Hopwater Distribution',
+    location: '850 Bush St. San Francisco, CA 94108',
+    lat: 37.7900197,
+    long: -122.4814119,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: "Jasper's Corner Tap & Kitchen",
+    location: '401 Taylor St. San Francisco, CA 94102',
+    lat: 37.7861056,
+    long: -122.48159,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'The Jug Shop',
+    location: '1567 Pacific Avenue San Francisco, CA 94109',
+    lat: 37.795231,
+    long: -122.420708,
+    rating: 0,
+    type: ['bottleshop']
+  },
+  {
+    name: 'LaTrappe Restaurant Bar',
+    location: '800 Greenwich St. San Francisco, CA 94133',
+    lat: 37.801886,
+    long: -122.412869,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Laughing Monk Brewing',
+    location: '1439 Egbert Ave, San Francisco, CA 94124',
+    lat: 37.7229731,
+    long: -122.4635255,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Liquid Gold',
+    location: '1040 Hyde St. San Francisco, CA 94109',
+    lat: 37.7906032,
+    long: -122.4193008,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: 'Local Brewing Co',
+    location: '69 Bluxome St. San Francisco, CA 94107',
+    lat: 37.776469,
+    long: -122.4672,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Magnolia Pub and Brewery',
+    location: '1398 Haight Street San Francisco, CA 94117',
+    lat:  37.770130,
+    long: -122.445084,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Mikkeller Bar SF',
+    location: '34 Mason St. San Francisco, CA 94102',
+    lat: 37.783976,
+    long: -122.408981,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: "Monk's Kettle",
+    location: '1000 Great Highway San Francisco, CA 94103',
+    lat: 37.764880,
+    long: -122.422851,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'The Page',
+    location: '298 Divisadero San Francisco, CA 94117',
+    lat: 37.772189,
+    long: -122.437196,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Pi Bar',
+    location: '24 Willie Mays Plaza San Francisco, CA 94107',
+    lat: 37.749908,
+    long: -122.420748,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Public House',
+    location: '1000 Great Highway San Francisco, CA 94107',
+    lat: 37.778644,
+    long: -122.389380,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Richmond Republic Draught House',
+    location: '642 Clement St. San Francisco, CA 94118',
+    lat: 37.7830912,
+    long: -122.536291,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Rogue Ales Public House',
+    location: '673 Union St. San Francisco, CA 94133',
+    lat: 37.800385,
+    long: -122.410385,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: 'Sessions at the Presidio',
+    location: '1 Letterman Dr. #150 San Francisco, CA 94129',
+    lat: 37.7992595,
+    long: -122.5179878,
+    rating: 0,
+    type: ['taproom', 'bottleshop']
+  },
+  {
+    name: 'Social Kitchen & Brewery',
+    location: '1326 9th Ave. San Francisco, CA 94122',
+    lat: 37.7634894,
+    long: -122.5361535,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Southern Pacific Brewing',
+    location: '620 Treat Ave. San Francisco, CA 94110',
+    lat: 37.7600819,
+    long: -122.4842658,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Speakeasy Ales and Lagers',
+    location: '1195 Evans Avenue, Suite A San Francisco, CA 94124',
+    lat: 37.738888,
+    long: -122.380913,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Sunset Reservoir Brewing Company',
+    location: '1735 Noriega St. San Francisco, CA 94122',
+    lat: 37.7537409,
+    long: -122.5524514,
+    rating: 0,
+    type: ['brewpub']
+  },
+  {
+    name: 'Suppenkuche',
+    location: '601 Hayse St. San Francisco, CA 94102',
+    lat: 37.776260,
+    long: -122.426285,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Thirsty Bear Brewing Company',
+    location: '661 Howard St. San Francisco, CA 94105',
+    lat: 37.785664,
+    long: -122.399605,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Toronado',
+    location: '547 Haight St. San Francisco, CA 94117',
+    lat: 37.772034,
+    long: -122.431084,
+    rating: 0,
+    type: ['taproom']
+  },
+  {
+    name: 'Triple Voodoo Brewery and Tap Room',
+    location: '2245 3rd St. San Francisco, CA 94107',
+    lat: 37.761190,
+    long: -122.388537,
+    rating: 0,
+    type: ['brewery']
+  },
+  {
+    name: 'Woods Cerveceria',
+    location: '3801 18th St. San Francisco, CA 94114',
+    lat: 37.7610878,
+    long: -122.4985094,
+    rating: 0,
+    type: ['brewpub']
+  },
+  {
+    name: 'Zeitgeist',
+    location: '199 Valencia St. San Francisco, CA 94103',
+    lat: 37.769928,
+    long: -122.422279,
+    rating: 0,
+    type: ['taproom']
+  }
+
+];
