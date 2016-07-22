4139f956623a9f74562e1fcdab3a43a9e3ede7cf
mattericbrown <matt.eric.brown@gmail.com>
2016-07-21 21:51:12 -0700

    added requirement for apiController.js in index.js so the server can access it

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,7 +1,7 @@
 function index(req, res) {
   res.json({
     message: "Welcome to beer.me!",
-    documentation_url: "https://github.com/mattericbrown/Brewfinder",
+    documentation_url: "https://github.com/mattericbrown/brewfinder",
     base_url: "https://beer-me-sf.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,3 @@
+module.exports.api = require('./apiController');
 module.exports.locations = require('./locationsController');
 module.exports.users = require('./usersController');
