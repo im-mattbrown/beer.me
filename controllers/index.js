dfefe346186dfe5cd0dcaa82bf3eac794a56de45
mattericbrown <matt.eric.brown@gmail.com>
2016-07-26 09:52:03 -0700

    changed title of Google Map in index.html

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 // var mongoose = require("mongoose");
-// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/brewfinder");
+// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/3000");
 
 module.exports.api = require('./apiController');
 module.exports.locations = require('./locationsController');
