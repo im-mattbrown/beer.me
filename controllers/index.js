9fff3f1c6eae07776a7d8be58a0462301d783061
mattericbrown <matt.eric.brown@gmail.com>
2016-07-26 10:16:11 -0700

    trying to deploy to heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
-// var mongoose = require("mongoose");
-// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/3000");
+var mongoose = require("mongoose");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/brewfinder");
 
 module.exports.api = require('./apiController');
 module.exports.locations = require('./locationsController');
