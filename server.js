e3bf513b3f14a0a7ba72309ab8139ffc6fdf15f4
mattericbrown <matt.eric.brown@gmail.com>
2016-07-26 10:18:49 -0700

    trying again to heroku

--- server.js
+++ server.js
@@ -39,4 +39,6 @@ app.put('/api/locations/:id', controllers.locations.update);
 app.get('/api/locations/:id/info', controllers.locations.info);
 
 //listen on port 3000
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
